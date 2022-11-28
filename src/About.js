0481bb26f0f968990d042347bf9459c8feefd4cb
melanieinpursuit <melaniegonzalez@pursuit.org>
2022-11-28 10:11:30 -0500

    attempting submit feature

--- src/About.js
+++ src/About.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import MyImage from './Pursuit 9.2 - Melanie Gonzalez 119.jpg'
 import MyImage2 from './D0CD96CA-567D-43BB-9B5B-CF4ED1F647A7IMG_3058.jpg'
 import './About.css'
@@ -11,12 +12,24 @@ export default function About () {
            <aside>
             <img src={MyImage} alt="Melanie's Profile"></img>
             <h2>Melanie Gonzalez</h2>
+            <a href="https://github.com/melanieinpursuit">
+            <p>GitHub</p>
+            </a>
+            <a href="https://linkedin.com/in/melanie-gonzalez-34b314245/">
+            <p>LinkedIn</p>
+            </a>
             <p>I am an upcoming Full Stack Web Developer currently training as a Pursuit Fellow. 
                 My current goal is to become a developer for a video game company and help create incredible gameplay as well as create new, relatable and diverse stories for those in marginalized communities, such as the LGBTQ+ community, women, women of color, disabled people, and so many others who don't have much representation in the gaming industry. </p>
            </aside>
            <aside>
             <img src={MyImage2} alt="Yianna's Profile"></img>
             <h2>Yianna Richey</h2>
+            <a href="https://github.com/yiannarichey">
+            <p>GitHub</p>
+            </a>
+            <a href='https://www.linkedin.com/in/yianna-richey-aa00ba15b/'>
+            <p>LinkedIn</p>
+            </a>
             <p>
             I am a Full Stack Software Engineer Fellow, studying at Pursuit. I am currently learning Javascript, CSS, and HTML, React, Node.js, and etc. I am pursuing Software Engineering due to it's alignment with my dream career to become a Front End Developer and Web Designer.
 
--- src/Home.js
+++ src/Home.js
@@ -23,22 +23,27 @@ export default function Home () {
    const [searchError, setSearchError] = useState(false)
    
    
-   function getAllVideos (title) {
-    const url = `${URL}?&maxResults=20&order=relevance&q=${title}&part=snippet&key=${key2}`
+   function getAllVideos () {
+    const url = `${URL}?&maxResults=20&order=relevance&q=${input}&part=snippet&key=${key2}`
+    console.log(url)
     return fetch(url)
     .then((res) => 
-    {return res.json()});
+     res.json()
+    );
+  
 }
+
    function handleChange (e) {
-    // e.preventDefault()
     const title = e.target.value;
     setInput(title)
-    
-   }
+}
 
-   function handleSubmit (){
-
-   }
+   function handleSubmit (e) {
+    e.preventDefault()
+getAllVideos()
+   setVideos("")
+    setInput("")
+}
 
    // setup a form 
    // then put an onsubmit in the form element 
@@ -47,40 +52,31 @@ export default function Home () {
    // at the end of the fetch need to do another .then to get the result 
    // can use line 17 setVideos to set the state
 
-//    useEffect(()=>{
-//    
-//         .catch((error)=> {
-//             console.log(error);
-//             setSearchError(true)
-//         })
-//    }, [])
     return (
         <div>
             { searchError ? (
-                    
                 <>  
                   <ErrorMessage/>
                 </>
                 ):(
-                    
                     <label htmlFor="searchBar" className="searchLabel">
                     Search: 
-                    <form> 
-                    <input
-                    type="text"
-                    value={input}
-                    id="input"
-                    onChange={handleChange}
-                    >
-                    </input>
+                        <form> 
+                            <input
+                            type="text"
+                            value={input}
+                            id="input"
+                            onChange={handleChange}
+                            >
+                            </input>
 
-                    <button
-                    type="submit"
-                    id="searchButton"
-                    onSubmit={getAllVideos(input)}>
-                        Search
-                    </button>
-                    </form>
+                            <button
+                            type="submit"
+                            id="searchButton"
+                            onSubmit={getAllVideos}>
+                            Search
+                            </button>
+                        </form>
                 </label> 
                 )
             }
